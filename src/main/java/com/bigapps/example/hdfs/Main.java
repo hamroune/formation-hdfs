b3518423688c6525ebc459bfe70e26b1f285aa1a
zahir <hamroune.z@gmail.com>
2018-09-30 11:18:30 +0200

    first commit

---
+++ src/main/java/com/bigapps/example/hdfs/Main.java
@@ -0,0 +1,22 @@
+package com.bigapps.example.hdfs;
+
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.net.URI;
+import java.util.logging.Logger;
+
+public class Main {
+
+   private static final Logger logger = Logger.getLogger("com.bigapps.example.hdfs.Main");
+
+   public static void main(String[] args) throws Exception {
+      logger.info("Starting ");
+
+   }
+}
