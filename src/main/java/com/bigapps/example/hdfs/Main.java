8fc6acbe31ec1718f92b41a748990473b0ad7666
bouazizimedridha <bouazizimedridha@hotmail.com>
2018-10-07 17:10:19 +0200

    test

--- src/main/java/com/bigapps/example/hdfs/Main.java
+++ src/main/java/com/bigapps/example/hdfs/Main.java
@@ -18,5 +18,18 @@ public class Main {
    public static void main(String[] args) throws Exception {
       logger.info("Starting  Mohamed");
 
+      //hdfs:"10.132.0.2:8020"
+   String hdfsUris = "hdfs://10.132.0.2:8020";
+      Configuration conf = new Configuration();
+// set FileSystem UTL
+      conf.set("fs.defaultFS", hdfsUris);
+//Because of Maven
+      conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+      conf.set("fs.hdfs.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+// Set Hadoop User
+      System.setProperty("HADOOP_USER_NAME", "hdfs");
+      System.setProperty("hadoop.home.dir", "/");
+      FileSystem fs = FileSystem.get(URI.create(hdfsUris),conf);
+
    }
 }
