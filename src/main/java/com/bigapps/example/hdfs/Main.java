b661227b8f3ef2d660e5c823b118c226c92f18f5
wissammk <bw_maamar_kouadri@esi.dz>
2018-10-07 16:05:28 +0200

    rien

--- src/main/java/com/bigapps/example/hdfs/Main.java
+++ src/main/java/com/bigapps/example/hdfs/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
       try {
 
-         String hdfsPath= "hdfs://10.132.0.2:8020/";
+         String hdfsPath= "hdfs://10.132.0.2:8020";
          String localFileName= "/home/bw_maamar_kouadri/helloworld.txt";
          System.out.println("okay");
          Configuration configuration = new Configuration();
@@ -35,7 +35,7 @@ public class Main {
          FileSystem fs = null;
          fs = FileSystem.get(new URI(hdfsPath), configuration);
          System.out.println("okay3");
-         fs.copyFromLocalFile(new Path(localFileName), new Path(hdfsPath));
+         fs.copyFromLocalFile(new Path(localFileName), new Path(hdfsPath+"/"));
          System.out.println("okay4");
 
 
